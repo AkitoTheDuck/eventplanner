132562c768471a26a30606a02163981057d7adfc
MaffimGit <maxim.steinhauer@web.de>
2025-03-25 09:19:10 +0100

    Laufzettel erstellen
    - Downloadpfade für Raumplan und Laufzettel updaten

--- src/FileWriter/CompanyTimeTableWriter.java
+++ src/FileWriter/CompanyTimeTableWriter.java
@@ -19,6 +19,7 @@ public class CompanyTimeTableWriter extends FileWriter <Company> {
 
         XSSFWorkbook workbook = new XSSFWorkbook();
         XSSFSheet sheet = workbook.createSheet("Raumplan");
+        String filePath = "U:\\Documents\\Downloads\\";
 
         IndexedColors grey = IndexedColors.GREY_25_PERCENT;
 
@@ -147,7 +148,7 @@ public class CompanyTimeTableWriter extends FileWriter <Company> {
 
         try {
 
-            FileOutputStream out = new FileOutputStream(new File("src/FileWriter/test1.xlsx"));
+            FileOutputStream out = new FileOutputStream(new File(filePath + "Raumplan.xlsx"));
             workbook.write(out);
             out.close();
             System.out.println("test1.xlsx written successfully on disk.");
--- src/FileWriter/Test.java
+++ src/FileWriter/Test.java
@@ -13,12 +13,12 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
 
-//        String filename3 = "ressources/import/2024/Veranstaltungen.xlsx";
-//        CompanyReader coReader = new CompanyReader(filename3);
-//        ArrayList<Company> companies =  coReader.parse();
-//
-//        FileWriter<Company> writer = new CompanyTimeTableWriter();
-//        writer.write(companies);
+        String filename3 = "ressources/import/2024/Veranstaltungen.xlsx";
+        CompanyReader coReader = new CompanyReader(filename3);
+        ArrayList<Company> companies =  coReader.parse();
+
+        FileWriter<Company> writer = new CompanyTimeTableWriter();
+        writer.write(companies);
 
         String filename4 = "ressources/import/2024/Wahl BOT 23_24.xlsx";
         StudentReader studentReader = new StudentReader(filename4);
